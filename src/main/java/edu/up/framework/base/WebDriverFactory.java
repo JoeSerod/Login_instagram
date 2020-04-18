3226f78da5110ad5b479483973771a28d8abc36c
CamiloSegovia <54286208+CamiloSegovia@users.noreply.github.com>
2020-04-18 12:12:58 -0500

    3

---
+++ src/main/java/edu/up/framework/base/WebDriverFactory.java
@@ -0,0 +1,17 @@
+package edu.up.framework.base;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+public class WebDriverFactory {
+    private final static Logger logger = LoggerFactory.getLogger(WebDriverFactory.class);
+
+}
